76c8d9539a9738b4c4afed5c75682ea64197af0a
FNTnemo <72974331+FNTnemo@users.noreply.github.com>
2023-04-15 18:57:15 +0300

    Initial commit

---
+++ src/main/java/me/fntnemo/minecraftinohio/Events/ExplosionLighter.java
@@ -0,0 +1,21 @@
+package me.fntnemo.minecraftinohio.Events;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class ExplosionLighter implements Listener {
+
+    @EventHandler
+    public void explosion(PlayerInteractEvent event){
+        Player player = event.getPlayer();
+        if(event.getAction() == Action.RIGHT_CLICK_BLOCK &&
+        player.getInventory().getItemInHand().getType() == Material.FLINT_AND_STEEL){
+            player.getWorld().createExplosion(event.getClickedBlock().getLocation(), 7);
+        }
+    }
+
+}
---
+++ src/main/java/me/fntnemo/minecraftinohio/Minecraft_in_Ohio.java
@@ -0,0 +1,20 @@
+package me.fntnemo.minecraftinohio;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Minecraft_in_Ohio extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        run();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public void run(){
+
+    }
+}
